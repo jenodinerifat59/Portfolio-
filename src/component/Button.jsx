10e0d9a1fc929b1e8be31525ed100cafc38bd821
jenodinerifat59 <mdjenodineislamrifat@gmail.com>
2025-11-12 22:56:32 +0600

    Normalize line endings after setting core.autocrlf

--- src/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({Items, className="",onClick}) => {
-  return (
-    <button onClick={onClick} className={`cursor-pointer hover:scale-110 transition-transform duration-300 ${className}`}>{Items}</button>
-  )
-}
-
-export default Button
